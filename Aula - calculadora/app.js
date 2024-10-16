a5afc3feea584c6099511b4a59d87203e8ecf2d4
EliasVicini <eliassvicini@hotmail.com>
2024-10-16 07:48:11 -0300

    upload files

---
+++ Aula - calculadora/app.js
@@ -0,0 +1,32 @@
+var calculadora = require("./Calculadora")
+
+
+console.log(calculadora.titulo);
+console.log(calculadora.mult(10,20));
+console.log(calculadora.sub(10,20));
+console.log(calculadora.div(10,20));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* var mostrarSite = true;
+const site = "www.vibedomomento.com.br";
+
+console.log("Hello World");
+console.log("Teste");
+
+if(mostrarSite){ // se mostrar site for true
+    console.log(site) // mostra site
+} */
+
